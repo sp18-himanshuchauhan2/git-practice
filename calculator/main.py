09a7e3570e7c68f4585218adc295368c8e7b81d6
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-18 17:44:08 +0530

    changed the values

--- calculator/main.py
+++ calculator/main.py
@@ -2,8 +2,8 @@ from calculator.add import add
 from calculator.sub import sub
 
 if __name__ == "__main__":
-    a = 10
-    b = 5
-
-    print("Add: ", add(a, b))
+    a = 15
+    b = 3
     print("Sub: ", sub(a, b))
+    print("Add: ", add(a, b))
+    
