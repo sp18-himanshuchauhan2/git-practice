9d1ed998450b2fde537bacc2d3c959606cea6887
unknown <shubham21491@iiitd.ac.in>
2025-06-18 17:30:12 +0530

    new methods have been imposed

--- calculator/main.py
+++ calculator/main.py
@@ -1,5 +1,7 @@
 from calculator.add import add
 from calculator.sub import sub
+from calculator.multiply import multiply
+from calculator.divide import divide
 
 if __name__ == "__main__":
     a = 10
@@ -7,3 +9,5 @@ if __name__ == "__main__":
 
     print("Add: ", add(a, b))
     print("Sub: ", sub(a, b))
+    print("Multiply: ", multiply(a,b))
+    print("Divide: ",divide(a,b))
